60e6548d3da55ffec8750d1d3224c9a5a6072b75
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-30 15:07:48 -0400

    Refactoring

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,81 +1,37 @@
 var totalPrice = 0;
 var totalGrams = 0;
 
-
-$.getJSON( "/computers", function( data ) {
-
+var populateTable = function(data){
   var items = [];
   $.each( data, function( i, obj ) {
 
-  totalPrice += parseFloat(obj.price);
-  totalGrams += parseFloat(obj.grams);
+	  totalPrice += parseFloat(obj.price);
+	  totalGrams += parseFloat(obj.grams);
 
-    items.push("<tr>" +
-                "<td>"+ obj.product_title + "</td>" +
-                "<td>"+ obj.title + "</td>" +
-                "<td>"+ Math.round((obj.grams / 1000)) + " kg" + "</td>" +
-                "<td>"+ "$" + obj.price + "</td>" +
-              "</tr>"
-              );
-
-  });
-
-$( "tbody" ).append( items );
-
-
-
-
-});
-
-$.getJSON( "/keyboards", function( data ) {
-
-  var items = [];
-  $.each( data, function( i, obj ) {
-
-  totalPrice += parseFloat(obj.price);
-  totalGrams += parseFloat(obj.grams);
-
-    items.push("<tr>" +
-                "<td>"+ obj.product_title + "</td>" +
-                "<td>"+ obj.title + "</td>" +
-                "<td>"+ Math.round((obj.grams / 1000)) + " kg" + "</td>" +
-                "<td>"+ "$" + obj.price + "</td>" +
-              "</tr>"
-              );
+	  items.push(
+	  	"<tr>" +
+	      "<td>"+ obj.product_title + "</td>" +
+	      "<td>"+ obj.title + "</td>" +
+	      "<td>"+ Math.round((obj.grams / 1000)) + " kg" + "</td>" +
+	      "<td>"+ "$" + obj.price + "</td>" +
+	    "</tr>"
+	   );
 
   });
 
 	$( "tbody" ).append( items );
+}
 
-
+var populateTotals = function(){
 	$( "tbody" ).append(
+		"<tr class='info'>" +
+      "<td>TOTAL:</td>" +
+      "<td></td>" +
+      "<td>"+ Math.round((totalGrams / 1000)) + " kg" +  "</td>" +
+      "<td>"+ "$" + (totalPrice.toFixed(2)) + "</td>" +
+    "</tr>"
+   );
+};
 
-
-							"<tr class='info'>" +
-                "<td>TOTAL:</td>" +
-                "<td></td>" +
-                "<td>"+ Math.round((totalGrams / 1000)) + " kg" +  "</td>" +
-                "<td>"+ "$" + (totalPrice.toFixed(2)) + "</td>" +
-              "</tr>"
-
-
-
-              );
-});
-
-
-
-    //   keyboards.forEach(function (keyboard) {
-    //     totalPrice += parseFloat(keyboard.price)
-    //     totalGrams += parseFloat(keyboard.grams);
-    //   });
-
-    //   computers.forEach(function (computer) {
-    //     totalPrice += parseFloat(computer.price)
-    //     totalGrams += parseFloat(computer.grams);
-    //   });
-
-    //   totalPrice = totalPrice.toFixed(2);
-    //   totalKilograms = (totalGrams / 1000).toFixed();
-
-
+$.getJSON( "/computers", function( data ) { populateTable(data); });
+$.getJSON( "/keyboards", function( data ) { populateTable(data); populateTotals(); });
