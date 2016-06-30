bfa74697aeae4b2fe1d3f98c2795d50b116e9fba
John Mazzucco <John.Mazzucco@gmail.com>
2016-06-30 09:32:35 -0400

    Push computers to a 'computers' array and oush keyboards to a 'keyboards' array

--- app.js
+++ app.js
@@ -19,8 +19,28 @@ let productsData = "";
   });
 
   res.on('end', (d) => {
-    let productsJson = JSON.parse(productsData).products[0];
-    console.log(productsJson);
+    let productsJson = JSON.parse(productsData).products;
+    // console.log(productsJson);
+
+
+    // Put calculations here then refactor them out
+
+    // Requirements:
+    // An equal number of computers and keyboards.
+    // As many different computer and keyboard variants the store has to offer, while buying no duplicate variants.
+    // Spend the least amount of money possible.
+    // Alice needs to know the total weight of all computers and keyboards so she can plan for enough volunteers to help carry it all.
+
+    let computers = [];
+    let keyboards = [];
+
+    // Remove everything but keyboards and computers from products object
+    for (var i in productsJson) {
+      let product = productsJson[i];
+      if (product.product_type === 'Keyboard') { keyboards.push(product) };
+      if (product.product_type === 'Computer') { computers.push(product) };
+    };
+
   });
 
 }).on('error', (e) => {
